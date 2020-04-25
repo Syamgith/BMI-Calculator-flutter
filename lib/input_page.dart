b4816b52627cb6eba7464cf325d66947b78f6846
syamgith <syamgithksudheesh@gmail.com>
2020-04-25 21:28:50 +0530

    added route to resultspage

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/results_page.dart';
 import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -194,11 +195,18 @@ class _InputPageState extends State<InputPage> {
                 ],
               ),
             ),
-            Container(
-              width: double.infinity,
-              color: kBottomContainerColor,
-              height: kBottomContainerHeight,
-              margin: EdgeInsets.only(top: 10.0),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ResultsPage()));
+              },
+              child: Container(
+                child: Text('Calculate'),
+                width: double.infinity,
+                color: kBottomContainerColor,
+                height: kBottomContainerHeight,
+                margin: EdgeInsets.only(top: 10.0),
+              ),
             ),
           ],
         ));
